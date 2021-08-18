bacb8bf9c1b086be4ee64700dc4afb2a92f3829e
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-08-18 18:56:22 -0400

    Typed global variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code ---- document & querySelector selects the id from index.html
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/////////////
+
+function writePassword () {
+  var choice = prompt("How many characters long would you like your password to be? (8-128 characters permited)");
+
+  //if (!choice) return;
+
+  if (choice >= 8) {
+    console.log(choice);
+  } else if (choice <= 8);
+  console.log("Please select a number from 8 - 128.");
+}
+
+//
+
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numner = "0123456789";
+var sybmbol = "!@#$%^&*_-+=?/";
+var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
