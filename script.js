a095f8fe69dfaae6f66833be1c79101a83da5cbb
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-08-19 20:25:37 -0400

    Added confirm conditional prompts as explained this morning

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
 var textArea = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
-var charactersAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=/";
-//var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//var charactersLower = "abcdefghijklmnopqrstuvwxyz";
-//var numbers = "0123456789";
-//var symbols = "!@#$%^&*()_+-=/";
+//var charactersAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=/";
+var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charactersLower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_+-=/";
+var charactersAll = [];
 
 function generatePassword() {
+  var password = "";
+  var possible = "";
+
   var choice = prompt("How many characters long would you like your password to be? (8-128 characters permited)");
   if (!choice) return;
-  //var password = ""; 
   if (choice < 8 || choice > 128 || isNaN(choice) /*&& parseInt(choice)*/) {
-  alert("Please enter a numeric value between 8 and 128 characters long.");
-  return generatePassword();
-} 
-/*  var choice = prompt("Would you like to have Upper Case letters in your password? yes or no");
-  if (!choice) return;
-  if (choice == "yes") {
-    var choice = prompt("Would you like to have Lower Case letters in your password?");
+    alert("Please enter a numeric value between 8 and 128 characters long.");
+    return generatePassword();
+  }
 
-  }   */
+  wantUpper = confirm("Would you like capital letters in your password?");
+  wantLower = confirm("Would you like lower case letters in your password?");
+  wantNumber = confirm("Would you like numbers in your password?");
+  wantSymbol = confirm("Would you like special characters in your password?");
 
-  var password = ""; 
-  for (var i = 0; i < choice; i++) {
-    var randomChar = charactersAll[Math.floor(Math.random() * 77)];
-    password += randomChar;
+  if (wantUpper) {
+    var randomChar = Math.floor(Math.random() * charactersUpper.length);
+    var option = charactersUpper[randomChar];
+    possible = possible.concat(charactersUpper);
+    charactersAll.push(option);
+  }
+
+  if (wantLower) {
+    var randomChar = Math.floor(Math.random() * charactersLower.length);
+    var option = charactersLower[randomChar];
+    possible = possible.concat(charactersLower);
+    charactersAll.push(option);
+  }
+
+  if (wantNumber) {
+    var randomChar = Math.floor(Math.random() * numbers.length);
+    var option = numbers[randomChar];
+    possible = possible.concat(numbers);
+    charactersAll.push(option);
+  }
+
+  if (wantSymbol) {
+    var randomChar = Math.floor(Math.random() * symbols.length);
+    var option = symbols[randomChar];
+    possible = possible.concat(symbols);
+    charactersAll.push(option);
+  }
+  
+  for (var i = 0; i < length - charactersAll.length; i++) {
+    var randomChar = Math.floor(Math.random() * possible.length);
+    password += possible[randomChar];
   }
   textArea.innerHTML = password;
+
+  password += charactersAll.join('');
+
+  console.log('POSSIBLE:', possible);
+  console.log('PASSWORD:', password);
+  console.log('STORED:', charactersAll);
+  charactersAll = [];
+
+  return password;
 }
 
 generateBtn.addEventListener("click", generatePassword);
