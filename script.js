b7c2c5ac6c92e5f3042882a6c9c5036d33b462a1
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-08-20 08:53:25 -0400

    simpllified all if statements and re-worked my for loop to take into account all/some if/confirm prompt choices

--- script.js
+++ script.js
@@ -1,70 +1,44 @@
 var textArea = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
-//var charactersAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=/";
 var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var charactersLower = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*()_+-=/";
-var charactersAll = [];
 
 function generatePassword() {
-  var password = "";
-  var possible = "";
-
+  
   var choice = prompt("How many characters long would you like your password to be? (8-128 characters permited)");
   if (!choice) return;
-  if (choice < 8 || choice > 128 || isNaN(choice) /*&& parseInt(choice)*/) {
+  if (choice < 8 || choice > 128 || isNaN(choice) /*|| parseInt(choice)*/) {
     alert("Please enter a numeric value between 8 and 128 characters long.");
     return generatePassword();
   }
 
-  wantUpper = confirm("Would you like capital letters in your password?");
-  wantLower = confirm("Would you like lower case letters in your password?");
-  wantNumber = confirm("Would you like numbers in your password?");
-  wantSymbol = confirm("Would you like special characters in your password?");
+  var wantUpper = confirm("Would you like capital letters in your password?");
+  var wantLower = confirm("Would you like lower case letters in your password?");
+  var wantNumber = confirm("Would you like numbers in your password?");
+  var wantSymbol = confirm("Would you like special characters in your password?");
 
-  if (wantUpper) {
-    var randomChar = Math.floor(Math.random() * charactersUpper.length);
-    var option = charactersUpper[randomChar];
-    possible = possible.concat(charactersUpper);
-    charactersAll.push(option);
-  }
+  var password = "";
+  var possible = "";
 
-  if (wantLower) {
-    var randomChar = Math.floor(Math.random() * charactersLower.length);
-    var option = charactersLower[randomChar];
-    possible = possible.concat(charactersLower);
-    charactersAll.push(option);
-  }
-
-  if (wantNumber) {
-    var randomChar = Math.floor(Math.random() * numbers.length);
-    var option = numbers[randomChar];
-    possible = possible.concat(numbers);
-    charactersAll.push(option);
-  }
-
-  if (wantSymbol) {
-    var randomChar = Math.floor(Math.random() * symbols.length);
-    var option = symbols[randomChar];
-    possible = possible.concat(symbols);
-    charactersAll.push(option);
-  }
+  if (wantUpper) possible += charactersUpper;
+  if (wantLower)  possible += charactersLower;
+  if (wantNumber)  possible += numbers;
+  if (wantSymbol)  possible += symbols;
   
-  for (var i = 0; i < length - charactersAll.length; i++) {
-    var randomChar = Math.floor(Math.random() * possible.length);
-    password += possible[randomChar];
+  // can also keep it simple w/ if (!possible) since these are the contents of the outcome "possible".
+  if (!wantUpper && !wantLower && !wantNumber && !wantSymbol) {
+    alert("Well, your password has to contain something silly... try again.");
+    return generatePassword();
+  }  
+  
+  for (var i = 0; i < choice; i++) {
+    var randomChar = possible.charAt(Math.floor(Math.random() * possible.length));
+    password += randomChar;
   }
+
   textArea.innerHTML = password;
-
-  password += charactersAll.join('');
-
-  console.log('POSSIBLE:', possible);
-  console.log('PASSWORD:', password);
-  console.log('STORED:', charactersAll);
-  charactersAll = [];
-
-  return password;
 }
 
 generateBtn.addEventListener("click", generatePassword);
